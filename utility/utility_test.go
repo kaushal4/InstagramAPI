0307e966c6e04f0cd55f3ee9c31205ce899e5283
Kaushal Damania <damaniakaushal4@gmail.com>
2021-10-09 19:58:31 +0530

    adding screenshot and bug fix

--- utility/utility_test.go
+++ utility/utility_test.go
@@ -5,18 +5,20 @@ import (
 )
 
 func TestWithIntegerString(t *testing.T) {
-	integerString := "2"
+	path := "/users/2"
+	base := "/users/"
 	integer := 2
 	var result int
-	result, err := GetIDFromString(integerString)
+	result, err := ExtractID(path, base)
 	if integer != result || err != nil {
 		t.Fatalf(`GetIDFromSting("2") = %d, %v, want match for %d, nil`, result, err, integer)
 	}
 }
 
 func TestWithInvalidString(t *testing.T) {
-	integerString := "A"
-	_, err := GetIDFromString(integerString)
+	path := "/users/A"
+	base := "/users/"
+	_, err := ExtractID(path, base)
 	if err == nil {
 		t.Fatalf(`Should have thrown an error`)
 	}
